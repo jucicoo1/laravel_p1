b4388b419bebc7f0bccd55d56634f57fb4ea0432
jucicoo1 <parkalex91@gmail.com>
2023-12-18 21:35:13 -0600

    cr

--- sample/app/Http/Resources/CommentResource.php
+++ sample/app/Http/Resources/CommentResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use function sprintf;
 
 class CommentResource extends JsonResource
 {
@@ -14,6 +15,14 @@ class CommentResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id'    =>  $this->resource['id'],
+            'body'  =>  $this->resource['body'],
+            '_links'    =>  [
+                'self'  =>  [
+                    'href'  => sprintf('https://example.com/%s', $this->resource['id']),
+                ]
+            ]
+        ];
     }
 }
