30f811404c9456c1d1902d150ff59e8c92b87697
jucicoo1 <parkalex91@gmail.com>
2024-01-04 11:27:55 -0600

    fake data file

--- sample/database/seeders/AuthorsTableSeeder.php
+++ sample/database/seeders/AuthorsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker;
 
 class AuthorsTableSeeder extends Seeder
 {
@@ -12,6 +13,14 @@ class AuthorsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        for($i=0;$i<10;$i++){
+            $author = [
+                'name'  =>  fake()->name(),
+                'created_at'    =>  now(),
+                'updated_at'    =>  now(),
+            ];
+            \Illuminate\Support\Facades\DB::table('authours')->insert($author);
+        }
+
     }
 }
