4a8cb1f2c9ffe871f01fed317874fb2d2d817edb
jucicoo1 <parkalex91@gmail.com>
2024-01-04 15:57:48 -0600

    breeze installed

--- sample/app/Models/Author.php
+++ sample/app/Models/Author.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+    protected $guarded = [
+        'id',
+        'created_at',
+    ];
+
+    public function __construct(){
+        $authors = \App\Models\Author::all();
+    }
 }
--- sample/routes/web.php
+++ sample/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,16 @@ Route::get('/', function () {
 
 Route::get('/home', function () {
     return view('home');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::get('/register', [App\Http\Controllers\RegisterController::class, 'create'])
@@ -47,3 +58,4 @@ Route::post('/user', [App\Http\Controllers\UserController::class, 'store']);
 
 // Route::get('/payload', 'ArticlePayloadAction');
 
+require __DIR__.'/auth.php';
