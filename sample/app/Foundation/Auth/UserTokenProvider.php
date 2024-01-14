be2d7d4051ca22007caaa30158e2c93b52401ab5
jucicoo1 <parkalex91@gmail.com>
2024-01-13 19:56:44 -0600

    UserTokenProvider

---
+++ sample/app/Foundation/Auth/UserTokenProvider.php
@@ -0,0 +1,15 @@
+<?
+declare(strict_types=1);
+
+namespace App\Foundation\Auth;
+
+use App\DataProvider\UserTokenProviderInterface;
+use App\Entity\User;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\UserProvider;
+use function is_null;
+
+final class UserTokenProvider implements UserProvider{
+    
+}
+?>
