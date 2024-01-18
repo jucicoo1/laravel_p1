e3018a8441ed79bac56720d8580cd6848f5a37d2
jucicoo1 <parkalex91@gmail.com>
2024-01-18 08:15:08 -0600

    edit AuthServiceProvider

--- sample/app/Providers/AuthServiceProvider.php
+++ sample/app/Providers/AuthServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\DataProvider\UserToken;
+use App\Foundation\Auth\UserTokenProvider;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +25,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // auth token
+        $this->registerPolicies();
+
+        $this->app->make('auth')->provider(
+            'user_token',
+            function (Application $app, array $config){
+                return new UserTokenProvider(new UserToken($app->make('db')));
+            }
+        );
     }
 }
